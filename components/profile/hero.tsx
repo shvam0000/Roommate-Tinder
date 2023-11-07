a0bb0ed4be8fa6c6982be99f3f197cc3c6005877
shvam0000 <shvam0000@gmail.com>
2023-11-07 17:02:57 -0500

    ✨ profile page

--- components/profile/hero.tsx
+++ components/profile/hero.tsx
@@ -49,23 +49,158 @@ const Hero = () => {
             />
           </div>
         </div>
-        <div>Price</div>
+        <div className="pt-2">Price</div>
         <div className="flex">
           <div>
             <label htmlFor="minPrice">min</label>
             <input
-              type="text"
+              type="number"
               className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
-              placeholder="Manhattan"
+              placeholder="$0"
             />
           </div>
           <div>
             <label htmlFor="age">max</label>
             <input
+              type="number"
+              className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
+              placeholder="$25"
+            />
+          </div>
+        </div>
+        <div className="py-2">
+          <div>
+            <label htmlFor="area">Interests</label>
+            <input
               type="text"
               className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
-              placeholder="25"
+              placeholder="Playing Squash, Reading"
             />
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-evenly pt-5">
+            <div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Morning Person
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Evening Person
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Drinking
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Smoking
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Pets
+                </label>
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Messy
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Clean
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Mixed Gender
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-[#F65B5B] bg-gray-100 border-gray-300 rounded  focus:ring-[#F65B5B] dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="link-checkbox"
+                  className="ml-2 text-sm font-medium text-[#F65B5B] pr-2 pl-0 py-2">
+                  Vegetarian
+                </label>
+              </div>
+            </div>
           </div>
         </div>
       </div>
