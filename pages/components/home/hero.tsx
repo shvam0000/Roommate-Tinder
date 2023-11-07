9c1d637827c08a9c67c6901d1b5b626619b944e3
shvam0000 <shvam0000@gmail.com>
2023-11-07 00:19:06 -0500

    ✨ add team section

--- pages/components/home/hero.tsx
+++ pages/components/home/hero.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Team } from '.';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <Team />
+    </div>
+  );
 };
 
 export default Hero;
--- pages/components/home/team.tsx
+++ pages/components/home/team.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+const team = [
+  {
+    name: 'Shivam Shekhar',
+    imgurl: 'https://avatars.githubusercontent.com/u/32397288?v=4',
+  },
+  {
+    name: 'Shivam Shekhar',
+    imgurl: 'https://avatars.githubusercontent.com/u/32397288?v=4',
+  },
+  {
+    name: 'Shivam Shekhar',
+    imgurl: 'https://avatars.githubusercontent.com/u/32397288?v=4',
+  },
+  {
+    name: 'Shivam Shekhar',
+    imgurl: 'https://avatars.githubusercontent.com/u/32397288?v=4',
+  },
+];
 
 const Team = () => {
-  return <div>Team</div>;
+  return (
+    <div>
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-bold py-5">Our Team</h1>
+      </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-4 gap-10 py-5">
+          {team.map((member) => (
+            <div
+              key={member.name}
+              className="flex flex-col justify-around items-center">
+              <img
+                className="rounded-full w-24 h-24"
+                src={member.imgurl}
+                alt={member.name}
+              />
+              <h1 className="text-lg font-bold">{member.name}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Team;
