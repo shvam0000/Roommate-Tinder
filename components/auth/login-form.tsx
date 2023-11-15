e5d742fdcbd5f5f1b1a75d1683b991dcf1c4a5b6
shvam0000 <shvam0000@gmail.com>
2023-11-15 00:54:56 -0500

    🎨 added login feature

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,64 +1,115 @@
-import React, { useState } from 'react';
+import { AccountContext } from '@/context/account';
+import { useRouter } from 'next/router';
+import React, { useContext, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ConfirmSignup from '@/pages/auth/confirmSignup';
 
 const AuthForm = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [userConfirmed, setUserConfirmed] = useState(true);
 
-  const loginHandler = () => {
-    alert('Form Submitted');
+  const router = useRouter();
+
+  const handleAWSError = (err: any) => {
+    if (err.code === 'NotAuthorizedException') {
+      const errorMessage = err.message || 'An unknown error occurred.';
+      // setErrorData(errorMessage);
+      //AWS error message with a toast message
+      toast.error(errorMessage);
+    }
+    if (err.code === 'UserNotConfirmedException') {
+      const errorMessage = err.message + 'Please verify your email.';
+      // setErrorData(errorMessage);
+      //AWS error message with a toast message
+      toast.error(errorMessage);
+      setUserConfirmed(false);
+    } else {
+      console.error(err);
+    }
+  };
+
+  const { authenticate } = useContext(AccountContext);
+
+  const loginHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    authenticate(email, password)
+      .then((data: any) => {
+        // check is the email is verified
+        console.log(data);
+        router.replace('/profile');
+        // We need to get all the user data from the Dynamo DB
+      })
+      .catch((err: any) => {
+        // console.error('error', err);
+        handleAWSError(err);
+      });
   };
 
   return (
-    <section className="pt-20 py-5">
-      <form onSubmit={loginHandler}>
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-          <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-white">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-[#F65B5B] md:text-2xl ">
-                Sign in to your account
-              </h1>
-              <div className="space-y-4 md:space-y-6">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-[#F65B5B]">
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
-                    placeholder="name@company.com"
-                    required
-                  />
+    <div>
+      {userConfirmed ? (
+        <section className="pt-20 py-5">
+          <form onSubmit={(event) => loginHandler(event)}>
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+              <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-white">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-[#F65B5B] md:text-2xl ">
+                    Sign in to your account
+                  </h1>
+                  <div className="space-y-4 md:space-y-6">
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-[#F65B5B]">
+                        Your email
+                      </label>
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
+                        placeholder="name@company.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-[#F65B5B]">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-[#F65B5B]  font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                      Sign in
+                    </button>
+                  </div>
                 </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-[#F65B5B]">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className=" border sm:text-sm rounded-lg block w-full p-2.5 bg-white border-gray-600 placeholder-gray-400"
-                    required
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-[#F65B5B]  font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                  Sign in
-                </button>
               </div>
             </div>
-          </div>
-        </div>
-      </form>
-    </section>
+          </form>
+        </section>
+      ) : (
+        <ConfirmSignup username={email} />
+      )}
+      <ToastContainer />
+    </div>
   );
 };
 
--- pages/auth/confirmSignup.tsx
+++ pages/auth/confirmSignup.tsx
@@ -3,10 +3,11 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { CognitoUser } from 'amazon-cognito-identity-js';
 import userPool from '@/context/user-pool/user-pool';
+import { Hero } from '@/components/profile';
 
 const ConfirmSignup = ({ username }: any) => {
   const [OTP, setOTP] = useState<string>('');
-  const [getUserDataProcess, setGetUserDataProcess] = useState(false);
+  const [profileProcess, setProfileProcess] = useState(false);
 
   console.log('username', username);
   console.log('userPool', userPool);
@@ -27,7 +28,7 @@ const ConfirmSignup = ({ username }: any) => {
         console.log(data);
         toast.success('Account verified successfully');
         // router.replace('/get-user-data');
-        setGetUserDataProcess(true);
+        setProfileProcess(true);
       }
     });
   };
@@ -45,57 +46,63 @@ const ConfirmSignup = ({ username }: any) => {
   };
 
   return (
-    <div className="relative w-full h-full py-32 min-h-screen">
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-white-100">
-            Confirm OTP sent on your email to Register
-          </h2>
-        </div>
+    <div>
+      {profileProcess === false ? (
+        <div className="relative w-full h-full py-32 min-h-screen">
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-white-100">
+                Confirm OTP sent on your email to Register
+              </h2>
+            </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            className="space-y-6"
-            action="#"
-            method="POST"
-            onSubmit={(event) => verifyAccount(event)}>
-            <div>
-              <label
-                htmlFor="OTP"
-                className="block text-sm font-medium leading-6 text-white-100">
-                Confirm OTP
-              </label>
-              <div className="mt-2">
-                <input
-                  id="OTP"
-                  name="otp"
-                  type="text"
-                  pattern="[0-9]*"
-                  inputMode="numeric"
-                  required={true}
-                  autoComplete="email"
-                  className="block border-b border-[#C6DE41] px-3 py-2 text-white-100 bg-transparent text-sm focus:outline-none focus-within:outline-none focus:ring-0 w-full ease-linear transition-all duration-150 sm:text-sm sm:leading-6"
-                  value={OTP}
-                  onChange={(e) => setOTP(e.target.value)}
-                />
-              </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form
+                className="space-y-6"
+                action="#"
+                method="POST"
+                onSubmit={(event) => verifyAccount(event)}>
+                <div>
+                  <label
+                    htmlFor="OTP"
+                    className="block text-sm font-medium leading-6 text-white-100">
+                    Confirm OTP
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="OTP"
+                      name="otp"
+                      type="text"
+                      pattern="[0-9]*"
+                      inputMode="numeric"
+                      required={true}
+                      autoComplete="email"
+                      className="block border-b border-[#C6DE41] px-3 py-2 text-white-100 bg-transparent text-sm focus:outline-none focus-within:outline-none focus:ring-0 w-full ease-linear transition-all duration-150 sm:text-sm sm:leading-6"
+                      value={OTP}
+                      onChange={(e) => setOTP(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  className="block text-sm font-medium leading-6 text-white-200 hover: hover:underline"
+                  onClick={resendConfirmationCode}>
+                  Resend Cofirmation Code
+                </button>
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-me-green-200 hover:bg-me-green-200/90 text-black px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Confirm OTP
+                  </button>
+                </div>
+              </form>
             </div>
-            <button
-              type="button"
-              className="block text-sm font-medium leading-6 text-white-200 hover: hover:underline"
-              onClick={resendConfirmationCode}>
-              Resend Cofirmation Code
-            </button>
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-me-green-200 hover:bg-me-green-200/90 text-black px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Confirm OTP
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
+      ) : (
+        <Hero />
+      )}
       <ToastContainer />
     </div>
   );
