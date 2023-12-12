43a0a4a68d45a2a373a8b118a9f501a4b463f279
shvam0000 <shvam0000@gmail.com>
2023-12-12 18:13:08 -0500

    🎨 update get-users to get multiple users

--- cloud/lambda-functions/rt-get-users.py
+++ cloud/lambda-functions/rt-get-users.py
@@ -1,9 +1,16 @@
 import json
+from decimal import Decimal
 import boto3
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
 
 def lambda_handler(event, context):
     region = 'us-east-1'
-    table_name = 'Roommate-Tinder'
+    table_name = 'Roommate-Tinder'  
     dynamodb = boto3.resource('dynamodb', region_name=region)
     table = dynamodb.Table(table_name)
     
@@ -19,7 +26,7 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps(formatted_items),
+        'body': json.dumps(formatted_items, cls=DecimalEncoder),
         'headers': {
             'Content-Type': 'application/json'
         }
