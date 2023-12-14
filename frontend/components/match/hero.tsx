c2776db7ec139cb24373a94f684d13e46bc7de16
shvam0000 <shvam0000@gmail.com>
2023-12-14 13:10:52 -0500

    add profiles from backend

--- frontend/components/match/hero.tsx
+++ frontend/components/match/hero.tsx
@@ -1,41 +1,71 @@
+//@ts-nocheck
 import man2 from '@/utils/images/man2.png';
 import Image from 'next/image';
 import dislike from '@/utils/images/dislike.png';
 import like from '@/utils/images/like.png';
 import Link from 'next/link';
-import { ChatList } from '.';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Hero = () => {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    axios(
+      'https://yclsvhn0s1.execute-api.us-east-1.amazonaws.com/roommate-tinder/users'
+    )
+      .then((res) => {
+        console.log(res.data.body);
+        setUsers(res.data.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="flex">
-      <div className="w-full">
-        <ChatList />
-      </div>
-      <div className="p-10 w-full">
-        <div className="bg-[#f8caca] mx-auto p-10 rounded-lg">
-          <figure className="flex justify-center">
-            <Image src={man2} alt="man2" />
-          </figure>
-          <div className="flex justify-center">
-            <Link href="/match">
-              <figure className="px-2 py-2">
-                <Image src={dislike} alt="dislike" />
-              </figure>
-            </Link>
-            <Link href="/match">
-              <figure className="px-2 py-2">
-                <Image src={like} alt="like" />
-              </figure>
-            </Link>
-          </div>
-          <h1 className="text-2xl font-bold">Sam, 28</h1>
-          <ul className="text-lg font-medium">
-            <li className="list-disc">Student at Columbia University</li>
-            <li className="list-disc">No Smoking, No Drinking</li>
-            <li className="list-disc">No Pets</li>
-            <li className="list-disc">Loves to Cook</li>
-          </ul>
-        </div>
+    <div className="flex justify-center items-center">
+      <div className="p-10">
+        {users &&
+          users?.map((user) => {
+            return (
+              <div key={user.id}>
+                <div className="bg-[#f8caca] mx-auto p-10 rounded-lg">
+                  <figure className="flex justify-center">
+                    <Image src={man2} alt="man2" />
+                  </figure>
+                  <div className="flex justify-center">
+                    <Link href="/match">
+                      <figure className="px-2 py-2">
+                        <Image src={dislike} alt="dislike" />
+                      </figure>
+                    </Link>
+                    <Link href="/match">
+                      <figure className="px-2 py-2">
+                        <Image src={like} alt="like" />
+                      </figure>
+                    </Link>
+                  </div>
+                  <h1 className="text-2xl font-bold">
+                    {user.firstName}, {user.age}, {user.gender}
+                  </h1>
+                  <ul className="text-lg font-medium">
+                    <li className="list-disc">Interests: {user.interests}</li>
+                    <li className="list-disc">
+                      {user.smoking === 'false_smoking'
+                        ? 'Non-Smoker'
+                        : 'Smoker'}
+                    </li>
+                    <li className="list-disc">
+                      {user.drinking !== 'false_drinking'
+                        ? 'Drinker'
+                        : 'Non-Drinker'}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
