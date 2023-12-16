734d90cb44d265d37b91e93a7e41d59e830aaeeb
shvam0000 <shvam0000@gmail.com>
2023-12-16 14:58:32 -0500

    temp: get matched users on FE

--- frontend/pages/chat/index.tsx
+++ frontend/pages/chat/index.tsx
@@ -1,11 +1,55 @@
 import { ProtectedRoute } from '@/components/auth';
 import { Layout } from '@/layout';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Chat = () => {
+  const [matchedUserProfiles, setMatchedUserProfiles] = useState([]);
+
+  useEffect(() => {
+    const id = localStorage.getItem(
+      'CognitoIdentityServiceProvider.va7i8r6ptmr6roqha7m6v09ke.LastAuthUser'
+    );
+
+    axios
+      .get(
+        `https://yclsvhn0s1.execute-api.us-east-1.amazonaws.com/roommate-tinder/user?id=${id}`
+      )
+      .then((res) => {
+        const matchUserIds = res.data.matches || [];
+        // Fetch profiles of liked users
+        return Promise.all(
+          matchUserIds.map((userId) =>
+            axios.get(
+              `https://yclsvhn0s1.execute-api.us-east-1.amazonaws.com/roommate-tinder/user?id=${userId}`
+            )
+          )
+        );
+      })
+      .then((responses) => {
+        const likedProfiles = responses.map((response) => response.data);
+        console.log(likedProfiles);
+        setMatchedUserProfiles(matchedUserProfiles); // Fix: set matchedUserProfiles instead of matchedUserProfiles
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []); // Fix: Remove matchedUserProfiles from dependency array
+
   return (
     <Layout>
-      <ProtectedRoute>Chat</ProtectedRoute>
+      <ProtectedRoute>
+        {/* Display user profiles */}
+        {matchedUserProfiles.map((profile) => (
+          <div key={profile.id}>
+            <h2>
+              {profile.firstName} {profile.lastName}
+            </h2>
+            <p>Email: {profile.email}</p>
+            {/* Add more fields as needed */}
+          </div>
+        ))}
+      </ProtectedRoute>
     </Layout>
   );
 };
